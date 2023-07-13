41648c1c2b9d01b4282d3fab8fcf0751762d1ecc
Olusoji <esohjay3@gmail.com>
2023-07-13 15:17:42 +0100

    add icons to nav

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,8 @@ import MapContainer from "@/components/map";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-5">
+    <main className="flex min-h-screen flex-col items-center p-5">
+      <h2>Greenspace finder</h2>
       <div className="h-[350px] w-[350px] p-2">
         <MapContainer
           mapOptions={{
