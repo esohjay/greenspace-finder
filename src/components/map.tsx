70cd4e4c0414061de2ccb3d916bb18e2cb51a30d
Olusoji <esohjay3@gmail.com>
2023-07-31 21:56:12 +0100

    add get features

--- src/components/map.tsx
+++ src/components/map.tsx
@@ -1,94 +1,30 @@
 "use client";
-import React, { useRef, useEffect } from "react";
+import React, { useRef, useEffect, useState } from "react";
 import Map from "@arcgis/core/Map";
 import WFSLayer from "@arcgis/core/layers/WFSLayer";
 import Graphic from "@arcgis/core/Graphic";
 import esriRequest from "@arcgis/core/request";
 import MapView from "@arcgis/core/views/MapView";
+import * as reactiveUtils from "@arcgis/core/core/reactiveUtils";
 import "@arcgis/core/assets/esri/themes/light/main.css";
+import { getUrl, getFeatures } from "@/lib/mapUtils";
 
 type MapDisplayProps = {
   mapOptions: __esri.MapProperties;
+  view?: MapView;
+  map?: Map;
+  // layer?: FeatureLayer;
+  savedExtent?: any;
 };
+
 function MapDisplay({ mapOptions }: MapDisplayProps) {
   const mapDiv = useRef<HTMLDivElement>(null!);
-
+  const [mapV, setMapV] = useState<MapView>();
+  const viewRef = useRef<__esri.MapView>();
   useEffect(() => {
-    function getUrl(params: Record<string, string>) {
-      const encodedParameters = Object.keys(params)
-        .map((paramName) => paramName + "=" + encodeURI(params[paramName]))
-        .join("&");
-
-      return "https://api.os.uk/features/v1/wfs?" + encodedParameters;
-    }
-    // Define a function to get features from the WFS
-    async function getFeatures(extent: __esri.Extent) {
-      // Convert the bounds to a formatted string.
-      const sw = extent.xmin + "," + extent.ymin;
-      const ne = extent.xmax + "," + extent.ymax;
-      const coords = sw + " " + ne;
-
-      // Create an OGC XML filter parameter value which will select the Greenspace
-      // features intersecting the BBOX coordinates.
-      let xml = "<ogc:Filter>";
-      xml += "<ogc:BBOX>";
-      xml += "<ogc:PropertyName>SHAPE</ogc:PropertyName>";
-      xml += '<gml:Box srsName="EPSG:3857">';
-      xml += "<gml:coordinates>" + coords + "</gml:coordinates>";
-      xml += "</gml:Box>";
-      xml += "</ogc:BBOX>";
-      xml += "</ogc:Filter>";
-      // Define (WFS) parameters object.
-      const wfsParams = {
-        key: "xjbXiGAwlVbHDEAAjBqz9RPxKOEy3lHy",
-        service: "WFS",
-        request: "GetFeature",
-        version: "2.0.0",
-        typeNames: "Zoomstack_Greenspace",
-        outputFormat: "GEOJSON",
-        srsName: "EPSG:3857",
-        filter: xml,
-      };
-
-      // const options = {
-      //   responseType: 'json'
-      // };
-
-      const url = getUrl(wfsParams);
-
-      return esriRequest(url, { responseType: "json" }).then(
-        (response: any) => response.data
-      );
-    }
-
     const initializeMap = async () => {
       try {
-        let xml = "<ogc:Filter>";
-        xml += "<ogc:BBOX>";
-        xml += "<ogc:PropertyName>SHAPE</ogc:PropertyName>";
-        xml += '<gml:Box srsName="EPSG:3857">';
-        // xml += '<gml:coordinates>' + coords + '</gml:coordinates>';
-        xml += "</gml:Box>";
-        xml += "</ogc:BBOX>";
-        xml += "</ogc:Filter>";
-        // Define (WFS) parameters object.
-        const wfsParams = {
-          key: "xjbXiGAwlVbHDEAAjBqz9RPxKOEy3lHy",
-          service: "WFS",
-          request: "GetFeature",
-          version: "2.0.0",
-          typeNames: "Zoomstack_Greenspace",
-          outputFormat: "GEOJSON",
-          srsName: "EPSG:3857",
-          filter: xml,
-        };
-
-        // Watch for map view stationary event
-        const layer = new WFSLayer({
-          url: getUrl(wfsParams),
-        });
-
-        const map = new Map({ ...mapOptions, layers: [layer] });
+        const map = new Map({ ...mapOptions });
         const view = new MapView({
           container: "viewDiv", //mapDiv.current,
           map,
@@ -101,6 +37,8 @@ function MapDisplay({ mapOptions }: MapDisplayProps) {
             rotationEnabled: false,
           },
         });
+        setMapV(view);
+        viewRef.current = view;
 
         // Clean up the map and view when the component is unmounted
         return () => {
@@ -117,6 +55,18 @@ function MapDisplay({ mapOptions }: MapDisplayProps) {
     };
     initializeMap();
   }, [mapOptions]);
+  reactiveUtils.watch(
+    // getValue function
+    () => mapV?.ready,
+    // callback
+    async (updating) => {
+      const v = mapV?.extent!;
+      const ft = await getFeatures(v);
+      console.log(ft);
+    }
+  );
+  console.log(mapV?.extent, "3");
+  console.log(viewRef.current);
   return (
     <div
       id="viewDiv"
