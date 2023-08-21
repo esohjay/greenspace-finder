84c49a5dd456943c7e955f8bf58857a0cf57ca4b
Olusoji <esohjay3@gmail.com>
2023-08-21 19:05:37 +0100

    add places page

--- src/app/places/page.tsx
+++ src/app/places/page.tsx
@@ -1,7 +1,69 @@
+"use client";
 import React from "react";
+import Image from "next/image";
+import { useAppDispatch, useAppSelector } from "@/redux/hooks";
+import {
+  setFeatures,
+  selectFeatures,
+  selectMapCenter,
+  selectMapExtent,
+} from "@/redux/features/mapSlice";
+import useMapUtils from "@/hooks/useMapUtils";
 
-function Places() {
-  return <div>Places</div>;
+export default function Places() {
+  const features = useAppSelector(selectFeatures);
+  const dispatch = useAppDispatch();
+  const center = useAppSelector(selectMapCenter)!;
+  const extent = useAppSelector(selectMapExtent)!;
+  const { getFeatures, addGraphicsToMap } = useMapUtils();
+  const findAll = () => {};
+  const findByExt = async () => {
+    const feat = await getFeatures(extent, "0");
+    console.log(feat, center);
+    if (feat && feat.features.length > 0) {
+      const { featureCollection } = addGraphicsToMap(feat, center);
+      dispatch(setFeatures(featureCollection));
+      console.log(featureCollection);
+    }
+  };
+  console.log(features, "ff");
+  return (
+    <section>
+      {features && features?.features?.length > 0 ? (
+        features?.features.map((feature) => (
+          <a
+            href="#"
+            key={feature.properties.OBJECTID}
+            className="block rounded-lg p-4 shadow-sm shadow-indigo-100"
+          >
+            <Image
+              alt="Home"
+              width={200}
+              height={200}
+              src="https://images.unsplash.com/photo-1613545325278-f24b0cae1224?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1770&q=80"
+              className="h-56 w-full rounded-md object-cover"
+            />
+
+            <div className="mt-2">
+              <dl>
+                <div>
+                  <dt className="sr-only">Price</dt>
+
+                  <dd className="text-sm text-gray-500">$240,000</dd>
+                </div>
+
+                <div>
+                  <dt className="sr-only">Address</dt>
+
+                  <dd className="font-medium">{feature.properties.Type}</dd>
+                </div>
+              </dl>
+            </div>
+          </a>
+        ))
+      ) : (
+        <p>No Facility nearby</p>
+      )}
+    </section>
+  );
 }
-
-export default Places;
