3048a7551c0fd8caac8426cde1d860d710c0af92
Olusoji <esohjay3@gmail.com>
2023-09-20 16:52:33 +0100

    add input to location

--- src/app/location/page.tsx
+++ src/app/location/page.tsx
@@ -1,13 +1,20 @@
 import React from "react";
 import LocationMap from "@/components/locationMap";
+import { FaSearchLocation } from "react-icons/fa";
 
 function Location() {
   return (
     <section>
-      <article>
-        <h3>Select location</h3>
+      <article className="p-5 bg-mainColor text-white">
+        <h3 className="text-center mb-3 text-lg font-bold">Select location</h3>
+        <form className="flex gap-x-3 items-center">
+          <input type="text" className="w-full bg-secondaryColor p-2 rounded" />
+          <button className="text-white text-xl ">
+            <FaSearchLocation />
+          </button>
+        </form>
       </article>
-      <div className="h-[350px] w-[350px] p-2">
+      <div className="h-[350px] w-full p-2">
         <LocationMap
           mapOptions={{
             basemap: "streets-vector",
