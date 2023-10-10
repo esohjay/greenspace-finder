482d394bd71596853e074c6f4d2e81f38aac4f6e
Olusoji <esohjay3@gmail.com>
2023-10-10 10:51:55 +0100

    add email confrim

--- src/app/auth/register/auth-form.tsx
+++ src/app/auth/register/auth-form.tsx
@@ -68,14 +68,12 @@ function RegisterForm() {
     } catch (err) {
     } finally {
       dispatch(setStatus("idle"));
-      router.push("/location");
-      // router.push("/auth/register/confirmation");
+      router.push("/auth/register/confirmation");
     }
   };
   useEffect(() => {
     if (authUser) {
-      router.push("/location");
-      // router.push("/auth/register/confirmation");
+      router.push("/auth/register/confirmation");
     }
   }, [authUser, router]);
   return (
